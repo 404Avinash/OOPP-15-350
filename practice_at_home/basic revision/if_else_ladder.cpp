649427b9af29125df2f83644a7cbf05ef442d810
404Avinash <avinash1807007@gmail.com>
2026-09-13 23:02:05 +0530

    p1

--- practice_at_home/basic revision/if_else_ladder.cpp
+++ practice_at_home/basic revision/if_else_ladder.cpp
@@ -1,1 +1,22 @@
-hshshs
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int marks;
+    cin>> marks;
+
+    if(marks>=90){
+        cout<<"Excellent";
+    }
+    else if (marks>=70){
+        cout<<"Good";
+    }
+    else if(marks>=40){
+        cout<<"fair";
+    }
+
+else{ 
+    cout<<"bad";
+    }
+    return 0;
+}
