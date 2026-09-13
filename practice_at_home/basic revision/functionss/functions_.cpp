b55e70b20ffd32fc06382d4ba64e0511406173bf
404Avinash <avinash1807007@gmail.com>
2026-09-13 23:35:36 +0530

    p7

---
+++ practice_at_home/basic revision/functionss/functions_.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// void printNumbers(int a,int b){
+//     for(int i=a;i<b;i++){
+//         cout<<i<<" ";
+//     }
+//     cout<<"/n";
+// }
+
+int sumOfNumbers(int a, int b){
+    return a+b;
+}
+int main() {
+    // printNumbers(10,20);
+    sumOfNumbers(10,20);
+    return 0;
+}
